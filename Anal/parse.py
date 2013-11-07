efcedfcf66b9deb45d0034bc041b30b4a46a3b40
Huanchen X Zhang <huanche1@xia-router2.(none)>
2013-11-06 21:03:57 -0500

    xia server git test

--- Anal/parse.py
+++ Anal/parse.py
@@ -3,7 +3,7 @@ import fileinput
 
 data = "leaf 0x7f8ded16a040: 1 key, version c0000000, permutation 0:edcba987654321, parent (nil), prev (nil), next (nil) [ksuf x2]\n  http://1 = SUBTREE #0/137\n    leaf 0x7f8dec568040: 5 keys, version c0000000, permutation 04312:edcba98765, parent (nil), prev (nil), next (nil) [ksuf i2x4]\n      -hydropo = SUBTREE #0/137\n        leaf 0x7f8dec367040: 1 key, version c0000000, permutation 0:edcba987654321, parent (nil), prev (nil), next (nil) [ksuf x2]\n"
 
-#abandoning regular expression
+ #abandoning regular expression
 '''
 p = re.compile('[a-z]')
 m = p.match(data)
