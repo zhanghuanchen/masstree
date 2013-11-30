8d116dd57ad6e2e43a1df2609173e7163d3f4e2f
Huanchen Zhang <huanchenzhang@yahoo.com>
2013-11-30 17:23:05 -0500

    attemp to minimize massnode overhead 8

--- masstree_struct.hh
+++ masstree_struct.hh
@@ -830,7 +830,7 @@ public:
 
   uint32_t nkeys_;
   uint32_t size_;
-  //uint8_t* keylenx_;
+  uint8_t* keylenx_;
   //ikey_type* ikey0_;
   //leafvalue_type* lv_;
   //uint32_t* ksuf_pos_offset_;
@@ -838,7 +838,7 @@ public:
 
   massnode (uint32_t nkeys, uint32_t size)
     :node_base<P>(false), nkeys_(nkeys), size_(size) {
-    //keylenx_ = (uint8_t*)((char*)this + sizeof(massnode<P>));
+    keylenx_ = (uint8_t*)((char*)this + sizeof(massnode<P>));
 	//keylenx_ = (uint8_t*)content_[0];
     //ikey0_ = (ikey_type*)((char*)keylenx_ + nkeys_ * sizeof(uint8_t));
     //lv_ = (leafvalue_type*)((char*)ikey0_ + nkeys_ * sizeof(ikey_type));
